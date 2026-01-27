d3e7e37049b914de24f7486c3aa01a0a87739690
Nader Alfares <nna5040.psu@gmail.com>
2026-01-26 23:43:41 -0500

    ready for class

--- Sketchify/Sketchify/Views/DrawingElementView.swift
+++ Sketchify/Sketchify/Views/DrawingElementView.swift
@@ -15,33 +15,8 @@ struct DrawingElementView: View {
     private let lineWidth: CGFloat = 5
     
     var body: some View {
-        
-        switch element.tool {
-        case .circle:
-            if element.points.count >= 2 {
-                Ellipse()
-                    .stroke(element.color, lineWidth: lineWidth)
-                    .frame(width: element.boundingRect.cgRect.width, height: element.boundingRect.cgRect.height)
-                    .position(x: element.boundingRect.cgRect.midX, y: element.boundingRect.cgRect.midY)
-            }
-        case .square:
-            if element.points.count >= 2 {
-                Rectangle()
-                    .stroke(element.color, lineWidth: lineWidth)
-                    .frame(width: element.boundingRect.cgRect.width, height: element.boundingRect.cgRect.height)
-                    .position(x: element.boundingRect.cgRect.midX, y: element.boundingRect.cgRect.midY)
-            }
-        case .triangle:
-            if element.points.count >= 2 {
-                TriangleShape()
-                    .stroke(element.color, lineWidth: lineWidth)
-                    .frame(width: element.boundingRect.cgRect.width, height: element.boundingRect.cgRect.height)
-                    .position(x: element.boundingRect.cgRect.midX, y: element.boundingRect.cgRect.midY)
-            }
-        case .freeform:
-            FreeformShape(points: element.points.cgPointsCollection)
-                .stroke(element.color, lineWidth: lineWidth)
-        }
+        //TODO: - Present drawn elements
+        EmptyView()
     }
 }
 
