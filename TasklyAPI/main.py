c9302e8db8538d59c8c4038cb82d36b9adaeccd5
Nader Alfares <nna5040.psu@gmail.com>
2026-03-03 13:16:07 -0500

    end of class

--- TasklyAPI/main.py
+++ TasklyAPI/main.py
@@ -1,6 +1,34 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
+tasks = []
+
 app = FastAPI()
 
-# TODO:- 
+# TODO:-
+class Task(BaseModel):
+    id: str
+    title: str
+    description: str
+    completed: bool
+
+
+
+@app.get("/tasks")
+def get_tasks():
+    return {"tasks": tasks}
+
+@app.post("/tasks")
+def create_task(task: Task):
+    global tasks
+    tasks.append(task)
+    return {"msg": "Task created successfully"}
+
+
+@app.delete("/tasks/{task_id}")
+def delete_task(task_id: str):
+    global tasks
+    for i, t in enumerate(tasks):
+        if t.id == task_id:
+            tasks.pop(i)
+            return {"msg": "Task deleted successfully"}
